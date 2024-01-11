c6898d5263ace6456084349c1074eeff9bed426a
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-01-12 01:21:43 +0530

    init

---
+++ theme.config.jsx
@@ -0,0 +1,11 @@
+export default {
+  logo: <span>Courses by Frontend Hire</span>,
+  search: {
+    placeholder: 'Search',
+  },
+  primaryHue: 300,
+  primarySaturation: 100,
+  footer: {
+    text: <span>© Yarala Hruthik Reddy {new Date().getFullYear()}</span>,
+  },
+};
